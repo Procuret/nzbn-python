74dc41013b6cc9fd3759946c3c31386bc4c86ff0
hwjeremy <hugh.jeremy@gmail.com>
2021-12-09 17:35:51 +1100

    add error imports

--- nzbn/__init__.py
+++ nzbn/__init__.py
@@ -5,3 +5,7 @@ from nzbn.entity_status import EntityStatus
 from nzbn.entity_type import EntityType
 from nzbn.trading_name import TradingName
 from nzbn.time import NzbnTime
+
+from nzbn.errors.error import NzbnError
+from nzbn.errors.api import NzbnApiError
+from nzbn.errors.type_error import NzbnTypeError
